29856a346e8383a9a9684272f9daa5dffca7f6a1
xCthulhu <59405868+xcthulhuph@users.noreply.github.com>
2023-05-08 23:49:39 +0800

    Add files via upload

---
+++ lokolocal/loader.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $("#logo-wrapper").fadeOut("slow");
+  }, 4000);
+});
+
+// $(window).load(function() {
+//   $(".logo").fadeOut();
+// });
